f96f061a6d06c8b06de3b12347d1ae372b98f3b0
ahmeddakroury <ahmed.dakroury@student.giu-uni.de>
2025-11-11 09:19:13 +0200

    Update Student.h

--- Student.h
+++ Student.h
@@ -4,18 +4,24 @@
 #include "Person.h"
 #include <string>
 #using namespace std;
-//#write student class here
 
+class Student : public Person {
+private:
+    int yearLevel;
+    string major;
+public:
+    Student(){
+        yearLevel = 1;
+        major = "Undeclared";
+    }
 
+    Student(string n, int a, int y, string m) : Person(n, a), yearLevel(y), major(m) {}
 
-
-
-
-
-
-
-
-
-
+    void display() override {
+        Person::display();
+        cout << "Year Level: " << yearLevel << endl;
+        cout << "Major: " << major << endl;
+    }
+};
 
 #endif
