b80ac35c2dcf4fa22cf565ee717877cc41830635
ahmeddakroury <ahmed.dakroury@student.giu-uni.de>
2025-11-11 09:46:04 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -4,7 +4,40 @@
 #include <string>
 #include "Student.h"
 using namespace std;
-//#write your code here
+
+class Course {
+private:
+    string courseCode;
+    string courseName;
+    int maxStudents;
+    Student* students;
+    int currentStudents;
+public:
+    Course(){
+        courseCode = "";
+        courseName = "";
+        maxStudents = 0;
+    }
+
+    Course(string cc , string cn , int maxS){
+        courseCode = cc;
+        courseName = cn;
+        maxStudents = maxS;
+    }
+
+    void addStudent(const Student& s){
+        if(currentStudents < maxStudents)
+            students.add(s);
+        else
+        return;
+
+    }
+
+    void displayCourseInfo(){
+            cout << "course: " << courseCode << "-" << courseName << endl;
+            cout << "max students: " << maxStudents << endl;
+            cout << "currently enrolled: " << students << endl;
+    }
 
 
 
