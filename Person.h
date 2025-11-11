6ca5aad0f158ffaee61fa32700af27e4a6466a2e
ahmeddakroury <ahmed.dakroury@student.giu-uni.de>
2025-11-11 09:14:18 +0200

    Implement Person class with constructors and display
    Added Person class with constructors and display method.

--- Person.h
+++ Person.h
@@ -2,19 +2,29 @@
 #define PERSON_H
 
 #include <string>
+#include <iostream>
 using namespace std;
 
-//#write Person class here
 
+class Person {
+private:
+    string name;
+    int id;
+public:
+    Person (){
+        this->name = "";
+        this->id = 0;
+     }
 
-
-
-
-
-
-
-
-
-
+     Person (string name, int id){
+        this->name = name;
+        this->id = id;
+     }
+     
+     void display(){
+        cout << "Name: " << name << ", ID: " << id << endl;
+     }  
+        
+}; 
 
 #endif
