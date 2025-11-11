aedccea8f21a347bbcd176662575ed3a125bbf39
ahmeddakroury <ahmed.dakroury@student.giu-uni.de>
2025-11-11 09:56:55 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,12 +5,18 @@
 #include "Instructor.h"
 #include "Course.h"
 
-// i implemented everyclass in its header , i came late and had no idea :( //
+// i implemented everyclass in its header , i came late and had no idea now i have no time:( //
 using namespace std;
 
 
 int main() {
-   
+   Student s = new Student("Omar Nabil" , 2202 , 2 , "CS");
+   Instructor i = new Instructor("Lina khaled" , 123 ,  "Informatics" , 5);
+   Course c = new Course("101" , "intro to prog" , 3);
+
+   c.displayCourseInfo();
+   s.display();
+   i.display();
     
     return 0;
 }
